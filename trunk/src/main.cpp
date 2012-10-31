b22fd078027944dcd1d0d5ad7b6f397ce1bcb9f0
cfield2 <cfield2@a1ed7df4-323d-e56b-f96e-97b70eb0cdd1>
2012-10-31 19:39:22 +0000

    Updated the comments.

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -2,7 +2,7 @@
  * main.cpp
  *
  * Created by: Christopher R. Field <christopher.field@nrl.navy.mil, cfield2@gmail.com>
- * Version: 3.0.0
+ * Version: 1.0.0
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
